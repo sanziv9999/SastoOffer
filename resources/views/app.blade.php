eecc7d0a61daf33cd45c83a9685743fe6eedd66d
sudip6164 <sudippradhanadgj@gmail.com>
2026-03-18 17:29:30 +0545

    fix: change dashboard favicon to SastoOffer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>SastoOffer</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
